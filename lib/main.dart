c199035010f259342662f6b5f93022c701b31de8
YUKI YAMAZAKI <gooo0523@gmail.com>
2022-01-16 13:19:26 +0900

    update|install_riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_manegement/screen/home.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final helloWorldProvider = Provider((_) => 'Hello world');
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
